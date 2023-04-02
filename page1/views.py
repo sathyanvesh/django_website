e0549d9ed919c0af79b07d9b3be57815ac7b47f1
sathyanvesh <burrasatyanvesh132@gmail.com>
2023-04-02 19:16:44 +0530

    changes reverted

--- page1/views.py
+++ page1/views.py
@@ -19,3 +19,15 @@ def DetailView(request,pk):
         'Product' : Product
     }
     return render(request,'detail.html',context)
+
+
+def add_product(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        desc = request.POST.get("desc")
+        image = request.FILES["upload"]
+        prod = Prodcuts(name=name,price=price,description=desc,image=image)
+        prod.save()
+    
+    return render(request,'addprod.html')
