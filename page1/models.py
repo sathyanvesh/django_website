8e6063256e7406aaf7e7e32402403e46187aae01
sathyanvesh <burrasatyanvesh132@gmail.com>
2023-04-02 19:04:33 +0530

    Modified

--- page1/models.py
+++ page1/models.py
@@ -6,7 +6,7 @@ class Prodcuts(models.Model):
     name = models.CharField(max_length=250)
     price = models.IntegerField()
     description = models.CharField(max_length=500)
-
+    image = models.ImageField(blank=True,upload_to='images')
 
     def __str__(self) :
         return self.name
--- page1/urls.py
+++ page1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateView,DetailView
+from .views import CreateView,DetailView,add_product
 
 app_name = 'page1'
 
 urlpatterns = [
     path('',CreateView),
     path('detail/<int:pk>',DetailView,name='detail'),
+    path('addprod/',add_product,name='addprod'),
 ]
